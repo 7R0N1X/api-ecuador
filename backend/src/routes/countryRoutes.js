65b8430553994897c85bfd477b95ef3bd9b3e360
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-20 09:38:33 -0500

    chore: remove frontend and backend files for cleanup

--- backend/src/routes/countryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const routes = express.Router()
-const countryControllers = require('../controllers/countryControllers')
-
-routes
-  .get('/', countryControllers.getInfo)
-  .post('/', countryControllers.createInfo)
-
-module.exports = routes
--- backend/src/routes/presidentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const routes = express.Router()
-const presidentControllers = require('../controllers/presidentControllers')
-
-routes
-  .get('/', presidentControllers.getInfo)
-  .post('/', presidentControllers.createInfo)
-
-module.exports = routes
