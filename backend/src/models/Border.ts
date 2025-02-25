36ccbfa29224baa831214092c8c52b18e701bd5e
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-25 09:48:51 -0500

    feat(models): add Border model with associations to countries

---
+++ backend/src/models/Border.ts
@@ -0,0 +1,8 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db";
+
+export const Border = sequelize.define("border", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+  name: { type: DataTypes.STRING, allowNull: false },
+  country_id: { type: DataTypes.INTEGER, allowNull: false },
+}, {tableName: "borders", timestamps: false});
