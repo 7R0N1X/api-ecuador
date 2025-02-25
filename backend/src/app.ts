63c03752a50a0eb89d234205050b042594991f4d
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-25 10:06:09 -0500

    feat(routes): integrate additional routes for countries, borders, languages, and regions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { regionRoutes } from "./routes/region.routes";
 import { provinceRoutes } from "./routes/province.routes";
 import { cantonRoutes } from "./routes/canton.routes";
 import { parishRoutes } from "./routes/parish.routes";
+import { countryRoutes } from "./routes/country.routes";
+import { languageRoutes } from "./routes/language.routes";
+import { borderRoutes } from "./routes/border.routes";
 
 export const app = express();
 
@@ -9,6 +13,10 @@ export const app = express();
 app.use(express.json());
 
 // Rutas
+app.use("/api", countryRoutes);
+app.use("/api", borderRoutes);
+app.use("/api", languageRoutes);
+app.use("/api", regionRoutes);
 app.use("/api", provinceRoutes);
 app.use("/api", cantonRoutes);
 app.use("/api", parishRoutes);
