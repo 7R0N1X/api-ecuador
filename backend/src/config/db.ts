39d0862caebdfb2d32eb3b61b8296cf53748c877
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-25 16:02:13 -0500

    feat(db): refactor database connection to use MYSQL_URL environment variable

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+export const sequelize = new Sequelize(process.env.MYSQL_URL!, {
   dialect: "mysql",
   logging: false,
 });
@@ -12,7 +8,7 @@ export const sequelize = new Sequelize({
 export const db = async () => {
   try {
     await sequelize.authenticate();
-    // await sequelize.sync({force: true});
+    await sequelize.sync({ force: true });
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
