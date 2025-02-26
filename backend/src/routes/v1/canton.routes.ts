3e7ccb2a3eeedf7ad0ca705f7b2ba6edb46eccb3
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-26 13:00:51 -0500

    fix(canton): update getCantons route to use async/await for improved handling

--- backend/src/routes/v1/canton.routes.ts
+++ backend/src/routes/v1/canton.routes.ts
@@ -3,7 +3,7 @@ import { getCantons } from "../../controllers/v1/canton.controller";
 
 export const cantonRoutes = Router();
 
-cantonRoutes.get("/cantons", (req, res) => {
-  const cantones = getCantons();
+cantonRoutes.get("/cantons", async (req, res) => {
+  const cantones = await getCantons();
   res.json(cantones);
 });
