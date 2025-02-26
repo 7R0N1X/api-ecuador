d2aee0364405b3820961f6d897b471999b4d3417
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-26 13:00:58 -0500

    fix(parish): update getParishes route to use async/await for improved handling

--- backend/src/routes/v1/parish.routes.ts
+++ backend/src/routes/v1/parish.routes.ts
@@ -3,7 +3,7 @@ import { getParishes } from "../../controllers/v1/parish.controller";
 
 export const parishRoutes = Router();
 
-parishRoutes.get("/parishes", (req, res) => {
-  const parroquias = getParishes();
+parishRoutes.get("/parishes", async (req, res) => {
+  const parroquias = await getParishes();
   res.json(parroquias);
 });
