04c904b6e683881ed0b2fd799a4d327ccf67eced
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-26 12:58:42 -0500

    fix(border): allow country_id to be nullable in Border model

--- backend/src/models/v1/Border.ts
+++ backend/src/models/v1/Border.ts
@@ -4,5 +4,5 @@ import { sequelize } from "../../config/db";
 export const Border = sequelize.define("border", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
   name: { type: DataTypes.STRING, allowNull: false },
-  country_id: { type: DataTypes.INTEGER, allowNull: false },
+  country_id: { type: DataTypes.INTEGER, allowNull: true },
 }, {tableName: "borders", timestamps: false});
