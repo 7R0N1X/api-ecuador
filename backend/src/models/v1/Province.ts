bf165c8e350392bdcfd02aab3dbebb40163a3783
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-26 13:00:17 -0500

    refactor(province): simplify id definition and reorder imports in Province model

--- backend/src/models/v1/Province.ts
+++ backend/src/models/v1/Province.ts
@@ -1,16 +1,11 @@
+import { Canton } from "./Canton";
 import { DataTypes } from "sequelize";
 import { sequelize } from "../../config/db";
-import { Canton } from "./Canton";
 
 export const Province = sequelize.define(
   "Province",
   {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
     name: { type: DataTypes.STRING, allowNull: false, unique: true },
     capital_id: { type: DataTypes.INTEGER, allowNull: true },
     region_id: { type: DataTypes.INTEGER, allowNull: true },
