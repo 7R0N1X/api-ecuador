74aeb25c3c441e1e6c42a20ed7d7c1a6f7928950
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-26 12:59:05 -0500

    fix(languages): allow country_id to be nullable in Languages model

--- backend/src/models/v1/Languages.ts
+++ backend/src/models/v1/Languages.ts
@@ -4,5 +4,5 @@ import { sequelize } from "../../config/db";
 export const Languages = sequelize.define("languages", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
   name: { type: DataTypes.STRING, allowNull: false },
-  country_id: { type: DataTypes.INTEGER, allowNull: false },
+  country_id: { type: DataTypes.INTEGER, allowNull: true },
 }, { tableName: "languages", timestamps: false });
